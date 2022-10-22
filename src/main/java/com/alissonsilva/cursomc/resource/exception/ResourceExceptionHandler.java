1478c489d2fdea71978f53ef3c0dc5d099b864bd
unknown <alissonjosef@gmail.com>
2022-10-21 22:46:11 -0300

    Delete

--- src/main/java/com/alissonsilva/cursomc/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/alissonsilva/cursomc/resource/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.alissonsilva.cursomc.services.exceptions.DataIntegretyException;
 import com.alissonsilva.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,13 @@ public class ResourceExceptionHandler {
 		
 		StanderdError err = new StanderdError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegretyException.class)
+	public ResponseEntity<StanderdError> dataIntegrety(DataIntegretyException e, HttpServletRequest request){
+		
+		StanderdError err = new StanderdError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
 }
--- src/main/java/com/alissonsilva/cursomc/services/CateogiraService.java
+++ src/main/java/com/alissonsilva/cursomc/services/CateogiraService.java
@@ -3,10 +3,12 @@ package com.alissonsilva.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.alissonsilva.cursomc.domain.Categoria;
 import com.alissonsilva.cursomc.repositories.CategoriaRepository;
+import com.alissonsilva.cursomc.services.exceptions.DataIntegretyException;
 import com.alissonsilva.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,9 +18,9 @@ public class CateogiraService {
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElseThrow(() -> new ObjectNotFoundException( 
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria obj) {
@@ -31,4 +33,12 @@ public class CateogiraService {
 		return repo.save(obj);
 	}
 
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegretyException("Não e possivel excluir uma categoria que possui produtos");
+		}
+	}
 }
